3250d16d4768ae7c3f6a97dbf16dd608b9315e74
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-15 14:15:51 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,12 @@ const userRouter = require('./userRouter')
 const brandRouter = require('./brandRouter')
 const typeRouter = require('./typeRouter')
 const reviewRouter = require('./reviewRouter')
-const orderRouter = require('./orderRouter')  // Новый роутер для заявок
+
 
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
 router.use('/brand', brandRouter)
 router.use('/device', deviceRouter)
 router.use('/review', reviewRouter)
-router.use('/order', orderRouter)  // Добавлено использование роутера для заявок
 
 module.exports = router
