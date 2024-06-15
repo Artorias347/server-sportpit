fc4fb0c6fbff41b2cff7e03b3922e33ac4aeb1de
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-15 22:30:35 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
-const Router = require('express')
-const router = new Router()
-const deviceRouter = require('./deviceRouter')
-const userRouter = require('./userRouter')
-const brandRouter = require('./brandRouter')
-const typeRouter = require('./typeRouter')
-const reviewRouter = require('./reviewRouter')
+const Router = require('express');
+const router = new Router();
+const deviceRouter = require('./deviceRouter');
+const userRouter = require('./userRouter');
+const brandRouter = require('./brandRouter');
+const typeRouter = require('./typeRouter');
+const reviewRouter = require('./reviewRouter');
+const orderRouter = require('./orderRouter'); // Добавлен роутер для заказов
+const basketRouter = require('./routes/basketRouter');
 
+router.use('/user', userRouter);
+router.use('/type', typeRouter);
+router.use('/brand', brandRouter);
+router.use('/device', deviceRouter);
+router.use('/review', reviewRouter);
+router.use('/order', orderRouter); // Использование роутера для заказов
+app.use('/api/basket', basketRouter);
 
-router.use('/user', userRouter)
-router.use('/type', typeRouter)
-router.use('/brand', brandRouter)
-router.use('/device', deviceRouter)
-router.use('/review', reviewRouter)
-
-module.exports = router
+module.exports = router;
