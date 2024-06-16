26ea46a46b2e8698fcd42ac4bc53e3fed50e7194
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-16 13:56:56 +0500

    Update orderRouter.js

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,9 +1,12 @@
-const Router = require('express');
-const router = new Router();
-const orderController = require('../controllers/orderController');
-const authMiddleware = require('../middleware/authMiddleware');
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController'); // Убедитесь, что путь к контроллеру правильный
+const authMiddleware = require('../middleware/authMiddleware'); // Убедитесь, что путь к middleware правильный
 
-router.post('/', authMiddleware, orderController.createOrder);
-router.get('/', authMiddleware, orderController.getOrders);
+// Определение маршрута для создания заказа
+router.post('/create', authMiddleware, orderController.createOrder);
+
+// Определение маршрута для получения заказов пользователя
+router.get('/my-orders', authMiddleware, orderController.getOrders);
 
 module.exports = router;
