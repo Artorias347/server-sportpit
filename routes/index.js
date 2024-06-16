c21acd94b82f11df99839a11ec668052eb3eddd7
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-16 12:12:48 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const brandRouter = require('./brandRouter');
 const typeRouter = require('./typeRouter');
 const reviewRouter = require('./reviewRouter');
 const orderRouter = require('./orderRouter'); // Добавлен роутер для заказов
-const basketRouter = require('./basketRouter');
+const basketRouter = require('./routes/basketRouter');
 
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
@@ -14,6 +14,6 @@ router.use('/brand', brandRouter);
 router.use('/device', deviceRouter);
 router.use('/review', reviewRouter);
 router.use('/order', orderRouter); // Использование роутера для заказов
-router.use('/api/basket', basketRouter);
+router.use('/cart', basketRouter);
 
 module.exports = router;
