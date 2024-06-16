daebd8a70d6c36ef846b21eb17c7011c0adc95f3
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-16 13:57:17 +0500

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,4 @@
-const { Order, Product } = require('../models/models');
+const { Order, Product } = require('../models');
 
 class OrderController {
     async createOrder(req, res) {
@@ -6,10 +6,8 @@ class OrderController {
         const userId = req.user.id;
 
         try {
-            // Создание заказа в базе данных
             const order = await Order.create({ userId, name, address, email });
 
-            // Обработка товаров в корзине и учет их в заказе
             for (const item of cart) {
                 const product = await Product.findByPk(item.id);
 
@@ -17,24 +15,32 @@ class OrderController {
                     return res.status(404).json({ message: `Товар с id ${item.id} не найден` });
                 }
 
-                // Проверка наличия достаточного количества товара на складе
                 if (product.quantity < item.quantity) {
                     return res.status(400).json({ message: `Недостаточное количество товара ${product.name}` });
                 }
 
-                // Уменьшение количества товара на складе
                 product.quantity -= item.quantity;
                 await product.save();
 
-                // Добавление товара в заказ с указанием количества
                 await order.addProduct(product, { through: { quantity: item.quantity } });
             }
 
-            // Успешный ответ с созданным заказом
             return res.status(201).json(order);
         } catch (error) {
             console.error('Не удалось создать заказ:', error);
             return res.status(500).json({ message: 'Не удалось создать заказ', error });
+        }
+    }
+
+    async getOrders(req, res) {
+        const userId = req.user.id;
+
+        try {
+            const orders = await Order.findAll({ where: { userId } });
+            return res.json(orders);
+        } catch (error) {
+            console.error('Не удалось получить заказы:', error);
+            return res.status(500).json({ message: 'Не удалось получить заказы', error });
         }
     }
 }
