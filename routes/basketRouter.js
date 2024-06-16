b980d9bbb7f1c9569738f328b255189a454f4887
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-16 12:12:37 +0500

    Update basketRouter.js

--- routes/basketRouter.js
+++ routes/basketRouter.js
@@ -5,5 +5,6 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/add', authMiddleware, basketController.addToBasket);
 router.delete('/remove/:basketDeviceId', authMiddleware, basketController.removeFromBasket);
+router.get('/', authMiddleware, basketController.getBasket);
 
 module.exports = router;
