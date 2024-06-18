b140dc9836685a9d1798c4ded8530e464658a1bf
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-18 21:07:22 +0500

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,9 +6,9 @@ module.exports = function (req, res, next) {
     }
     try {
         const token = req.headers.authorization.split(' ')[1] // Bearer asfasnfkajsfnjk
-        //if (!token) {
-        //    return res.status(401).json({message: "Не авторизован"})
-        //}
+        if (!token) {
+            return res.status(401).json({message: "Не авторизован"})
+        }
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
         req.user = decoded
         next()
