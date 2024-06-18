82fd0b7a3dcd721ecb8b69c9341d3626431517e6
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-18 22:29:31 +0500

    Update and rename reviewRouter.js to feedbackRouter.js

--- routes/reviewRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const reviewController = require('../controllers/reviewController')
-
-router.post('/', reviewController.createReview)
-router.get('/', reviewController.getAllReviews)
-
-module.exports = router 
